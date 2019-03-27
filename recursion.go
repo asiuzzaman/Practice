42fe0d8cf542add75a485a55cb9dde55a830929c
asiuzzaman006 <zaman1304006@gmail.com>
2019-03-28 01:18:51 +0600

    ReverseTheCharacter

---
+++ recursion.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+var s string = "asiuzzaman"
+
+func reverse(n int) {
+
+	if n == 10 {
+		return
+	}
+	n++
+	reverse(n)
+
+	fmt.Println(s[n-1])
+}
+
+func main() {
+
+	reverse(0)
+}
