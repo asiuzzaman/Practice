5226d20bb019eb8b032bc43e0202c56a18ab567e
asiuzzaman006 <zaman1304006@gmail.com>
2019-03-28 16:06:47 +0600

    GoroutineAvoidRaceCondition

--- goroutine3.go
+++ goroutine3.go
@@ -7,15 +7,20 @@ import (
 
 var x = 0
 
-func increment(wg *sync.WaitGroup) {
+func increment(wg *sync.WaitGroup, m *sync.RWMutex) {
+	m.Lock()
 	x++
+	m.Unlock()
 	wg.Done()
+
 }
+
 func main() {
 	var w = sync.WaitGroup{}
+	var m = sync.RWMutex{}
 	for i := 0; i < 1000; i++ {
 		w.Add(1)
-		go increment(&w)
+		go increment(&w, &m)
 	}
 
 	w.Wait()
