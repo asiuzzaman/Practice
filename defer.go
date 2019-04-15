10eb67366f0dea10e9316556b1dd69b70f679937
zamanItech <zaman@setuptech.international>
2019-04-15 18:43:16 +0600

    Defer..

---
+++ defer.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type rect struct {
+	length int
+	width  int
+}
+
+func (r rect) area(wg *sync.WaitGroup) {
+	if r.length < 0 {
+
+		fmt.Printf("rect %v's length should be greater than zero\n", r)
+		wg.Done()
+		return
+	}
+
+	if r.width < 0 {
+		fmt.Printf("rect %v's width should be greater than zero\n", r)
+		wg.Done()
+		return
+	}
+
+	area := r.width * r.length
+	fmt.Printf("rect %v's area %d\n", r, area)
+
+	wg.Done()
+
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	r1 := rect{-67, 89}
+	r2 := rect{5, -44}
+	r3 := rect{4, 66}
+
+	rects := []rect{r1, r2, r3}
+
+	for _, v := range rects {
+		wg.Add(1)
+
+		go v.area(&wg)
+	}
+	wg.Wait()
+
+	fmt.Println("All go routine finished")
+
+	wg.Wait()
+
+}
