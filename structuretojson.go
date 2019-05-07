006da4391f5acbc8219f1f12a9cb4e53f1c56660
zamanItech <zaman@setuptech.international>
2019-05-07 11:40:16 +0600

    Encode structure to json

---
+++ structuretojson.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
+
+type FruitBasket struct {
+	Name    string
+	Fruit   []string
+	Id      int64 `json:"ref"`
+	private string
+	Created time.Time
+}
+
+func main() {
+	basket := FruitBasket{
+		Name:    "Standard",
+		Fruit:   []string{"apple", "Banana", "Orange"},
+		Id:      999,
+		private: "second-rate",
+		Created: time.Now(),
+	}
+	var jsonData []byte
+	jsonData, err := json.Marshal(basket)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(string(jsonData))
+}
